3b94349a6d609e48fc3d9183578485db0cb7a5e0
jrhender <johnhenderson45@gmail.com>
2019-05-21 08:05:10 -0700

    added all the teams to the TeamDropdown

--- superscreenshot/src/TeamDropdown.tsx
+++ superscreenshot/src/TeamDropdown.tsx
@@ -8,8 +8,37 @@ const TeamDropdown = React.memo<ITeamDropdownProps>((props: ITeamDropdownProps) 
     return (
         <select onChange={(event : React.ChangeEvent<HTMLSelectElement>) => props.onTeamChange(event.target.value)}>
             <option value="">--Please choose an option--</option>
-            <option value="seahawks">Seahawks</option>
-            <option value="falcons">Falcons</option>
+            <option value="cardinals">Arizona Cardinals</option>
+            <option value="falcons">Atlanta Falcons</option>
+            <option value="ravens">Baltimore Ravens</option>
+            <option value="bills">Buffalo Bills</option>
+            <option value="bears">Chicago Bears</option>
+            <option value="seahawks">Cincinnati Bengals</option>
+            <option value="browns">Cleveland Browns</option>
+            <option value="cowboys">Dallas Cowboys</option>
+            <option value="broncos">Denver Broncos</option>
+            <option value="lions">Detroit Lions</option>
+            <option value="packers">Green Bay Packers</option>
+            <option value="texans">Houston Texans</option>
+            <option value="colts">Indianapolis Colts</option>
+            <option value="jaguars">Jacksonville Jaguars</option>
+            <option value="cheifs">Kansas City Chiefs</option>
+            <option value="dolphins">Miami Dolphins</option>
+            <option value="vikings">Minnesota Vikings</option>
+            <option value="patriots">New England Patriots</option>
+            <option value="saints">New Orleans Saints</option>
+            <option value="giants">New York Giants</option>
+            <option value="jets">New York Jets</option>
+            <option value="raiders">Oakland Raiders</option>
+            <option value="eagles">Philadelphia Eagles</option>
+            <option value="steelers">Pittsburgh Steelers</option>
+            <option value="rams">St. Louis Rams</option>
+            <option value="chargers">San Diego Chargers</option>
+            <option value="49ers">San Francisco 49ers</option>
+            <option value="seahawks">Seattle Seahawks</option>
+            <option value="buccaneers">Tampa Bay Buccaneers</option>
+            <option value="titans">Tennessee Titans</option>
+            <option value="redskins">Washington Redskins</option>
         </select>
     )
 });
