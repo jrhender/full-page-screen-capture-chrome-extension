5a747977036328ffb2c221e1e82dcc907e86c988
jrhender <johnhenderson45@gmail.com>
2019-06-27 18:12:45 -0700

    Initial implementation of csv gen from images

---
+++ GoogleCloudML/Generate_Image_CSV.py
@@ -0,0 +1,25 @@
+# Imports the Google Cloud client library
+from google.cloud import storage
+
+import csv
+
+# Instantiates a client
+storage_client = storage.Client()
+
+bucket_name = "sportseventdetection-football"
+bucket = storage_client.get_bucket(bucket_name)
+
+blobs = bucket.list_blobs()
+
+with open('sports_images.csv', mode='w') as sports_images_file:
+    sports_images_file = csv.writer(sports_images_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for blob in blobs:
+        image_name = blob.name
+        if image_name.endswith('png'):
+            print(blob.name)
+            if 'notTD' in image_name:
+                label = 'notTouchdown'
+            else:
+                label = 'touchdown'
+            sports_images_file.writerow(['gs://' + blob.name, label])
---
+++ GoogleCloudML/README.md
@@ -0,0 +1,6 @@
+#Setup of Python Client
+Used the article [here](https://cloud.google.com/storage/docs/reference/libraries)
+As per the article, the credentials are given to the python environment for the current shell session by using `export GOOGLE_APPLICATION_CREDENTIALS="[PATH]"`
+
+#Usage
+Run python to generate csv, manually upload to Google Cloud
